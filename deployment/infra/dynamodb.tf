f2e19063678580aff7132f03a770a6d482ea81ac
0000jill <106688710+0000jill@users.noreply.github.com>
2025-06-09 20:42:19 +0800

    Update dynamodb.tf

--- deployment/infra/dynamodb.tf
+++ deployment/infra/dynamodb.tf
@@ -16,3 +16,20 @@ resource "aws_dynamodb_table" "washer_status" {
     Environment = "prod"
   }
 }
+
+
+resource "aws_dynamodb_table" "user_info" {
+  name           = "team06-UserInfo"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "user_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  tags = {
+    Project     = "team06"
+    Environment = "prod"
+  }
+}
